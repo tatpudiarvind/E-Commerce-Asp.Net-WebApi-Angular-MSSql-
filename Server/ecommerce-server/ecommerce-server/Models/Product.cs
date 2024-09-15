c50fba74e940bdd6a4f6b3e885e7848da6bc6a35
tatpudiarvind <68744400+tatpudiarvind@users.noreply.github.com>
2024-09-15 19:46:28 +0530

    First time commit after project creation (#1) (#2)
    * First time commit after project creation
    
    * Code fix as per the comments

---
+++ Server/ecommerce-server/ecommerce-server/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ecommerce_server.Models;
+
+public partial class Product
+{
+    public int ProductId { get; set; }
+
+    public string ProductName { get; set; } = null!;
+
+    public string? Brand { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public decimal Mrp { get; set; }
+
+    public int Stock { get; set; }
+
+    public virtual Category? Category { get; set; }
+
+    public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+    public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
+}
---
+++ Server/ecommerce-server/ecommerce-server/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ecommerce_server.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string FullName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public string PasswordSalt { get; set; } = null!;
+
+    public string? Phone { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
+}
